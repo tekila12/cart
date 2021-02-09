3d6964bebffbe7c6dde2b3e0b0a520eb96820979
Ajnur Radovic <72083386+tekila12@users.noreply.github.com>
2021-02-09 13:25:39 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import CartContainer from './CartContainer'
+import {useGlobalContext} from './context'
+import Navbar from './Navbar'
+const App = () => {
+  const {loading} = useGlobalContext()
+  if(loading){
+    return (
+      <div className='loading'>
+      <h1> Loading...</h1>  
+      </div>
+    )
+    }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
+}
+
+export default App
+
